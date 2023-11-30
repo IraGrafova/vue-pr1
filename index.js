17a47509fa8099eb2541a2559d279d3f674d93e7
IraGrafova <i.grafova@ntc-hitech.ru>
2023-11-30 17:04:33 +0300

    animation

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 new Vue({
-    el: '#app',
-    data: {
-      type: "button",
-      isDisabled: true,
-      tasks: [
-        'Развернуть окружение в Codepen',
-        'Пройти курс по Vue',
-        'Сделать интернет-магазин на Vue',
-      ],
-      day: [
-        'Поработать',
-        'Забрать дочь из садика',
-        'Отдохнуть'
-      ]
-    }
-  })
+  el: "#app",
+  data: {
+    type: "button",
+    isDisabled: false,
+    tasks: [
+      { text: "Развернуть окружение в Codepen", done: true },
+      { text: "Пройти курс по Vue", done: false },
+      { text: "Сделать интернет-магазин на Vue", done: false },
+    ],
+    day: [
+      { text: "Поработать", done: true },
+      { text: "Забрать дочь из садика", done: false },
+      { text: "Отдохнуть", done: false },
+    ],
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({ text: this.message, done: false });
+      this.message = "";
+    },
+    count() {
+      return this.tasks.filter((task) => !task.done).length;
+    },
+
+    addTaskForDay() {
+      this.day.push({ text: this.message, done: false });
+      this.message = "";
+    },
+    countToday() {
+      return this.day.filter((task) => !task.done).length;
+    },
+  },
+});
+
+//почему не вывелся алерт
